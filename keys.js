569f6c350899a6a84e86e74e8541e0b5283fb403
David <dstillac@ncsu.edu>
2018-03-24 19:30:28 -0400

    setting up hw framework

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.kXlxSdCn9a4JBv8U1v0lQtGGo,
+  consumer_secret: process.env.44W9Uj5esQ3PaVFgfVpF5JsWJW7oB1YHWjRZmAT0los7e5sebB,
+  access_token_key: process.env.584051651-Dld0VMTtDirPSyJ0lTQqYVc92nNW4IhLrkwqrlEw,
+  access_token_secret: process.env.Xl5lgE4HWNokadxk7Ny4aed3PkScIsA8LwN4KWNkBgSWG
+};
+
+exports.spotify = {
+  id: process.env.27d72f62a6964da490057affe7940975,
+  secret: process.env.bb99d89e88e9493ca9e568f8a92bd5b7
+};

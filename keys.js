0733c89f6b246d5164890648f609b9fa01dd1280
David <dstillac@ncsu.edu>
2018-03-26 14:50:14 -0400

    Twitter

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.kXlxSdCn9a4JBv8U1v0lQtGGo,
-  consumer_secret: process.env.44W9Uj5esQ3PaVFgfVpF5JsWJW7oB1YHWjRZmAT0los7e5sebB,
-  access_token_key: process.env.584051651-Dld0VMTtDirPSyJ0lTQqYVc92nNW4IhLrkwqrlEw,
-  access_token_secret: process.env.Xl5lgE4HWNokadxk7Ny4aed3PkScIsA8LwN4KWNkBgSWG
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.27d72f62a6964da490057affe7940975,
-  secret: process.env.bb99d89e88e9493ca9e568f8a92bd5b7
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,4 +1,50 @@
+// Read and set any environment variables with the dotenv package
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// Load the external node modules 
+const Request = require('request');
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+
+// Load my keys.js file that contains the API keys
+const keys = require("./keys.js");
+
+// Load the file system module to allow files to be accessed
+const fs = require('fs');
+
+var liriRequest = process.argv[2];
+
+// Twitter API Requests ================================================================================
+function twitterRequest() {
+    
+    var client = new Twitter(keys.twitter);
+
+	client.get('statuses/user_timeline', function (error, tweets, response) {
+		if (!error) {
+            var tweetNumber = 1;
+
+            console.log("")
+            console.log("--------------------------------")
+            console.log("Here are 20 tweets:")
+            console.log("--------------------------------")
+            console.log("")
+            
+             for (var i = 0; i < tweets.length; i++) {
+				console.log("Date of tweet: " + tweets[i].created_at)
+                console.log("Tweet content: " + tweets[i].text)
+                console.log("Tweet number: " + tweetNumber++)
+                console.log("--------------------------------")
+                console.log("")
+            } 
+        }   else {
+            var twitterError = "Oops... something went wrong: " + error;
+			console.log(twitterError);
+			return;
+		}
+	});
+}
+
+// Run the requests appropriately if user's input matches
+if (liriRequest === "my-tweets") {
+    twitterRequest();
+}
